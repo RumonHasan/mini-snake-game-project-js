2d00cb7fbe3d83c11e82dd8a4c61c092f3548f23
MHRumon <rumonuniv@gmail.com>
2022-01-22 23:46:21 +0900

    food-position

--- components/food.js
+++ components/food.js
@@ -1,15 +1,20 @@
 // imports
 import { GAME_BOARD } from "../game.js";
+import { increaseSnakeParts, checkFoodCollision } from "./snake.js";
 
 // variables
-const food = { // main food object
+const SNAKE_INCREASE_RATE = 1;
+let food = { // main food object
     x: 10,
     y: 10
 }
 
 // update food
 export const updateFood = ()=>{
-    
+    if(checkFoodCollision(food)){
+        increaseSnakeParts(SNAKE_INCREASE_RATE)
+        food = randomFoodPositions();
+    }
 }
 
 // draw food
@@ -19,4 +24,13 @@ export const drawFood = (gameBoardElem)=>{
     foodElement.style.gridColumnStart = food.x;
     foodElement.style.gridRowStart = food.y;
     gameBoardElem.appendChild(foodElement);
+}
+
+// get random positions
+const randomFoodPositions = () =>{
+    let newFoodPositions;
+    return {
+        x: Math.floor(Math.random() * GAME_BOARD) + 1,
+        y: Math.floor(Math.random() * GAME_BOARD) + 1
+    }
 }
--- components/snake.js
+++ components/snake.js
@@ -6,18 +6,16 @@ const snakeBody = [{ // primary snake body array object
     x: 20,
     y: 10
 }];
-let snakeParts = 1; // initial snake segments
+let snakeParts = 0; // initial snake segments
 
 // update snake
 export const updateSnake = () => {
     updateSnakeSegments();
     const keyboardInput = getKeyboardControls();
-
     // moving the snake forward by switching the last and second last element back and forth
     for(let i = snakeBody.length - 2; i >= 0; i--){
         snakeBody[i + 1] = {...snakeBody[i]};
     }
-
     //adding 1 block in either x or y direction
     snakeBody[0].x += keyboardInput.x;
     snakeBody[0].y += keyboardInput.y;
@@ -34,6 +32,23 @@ export const drawSnake = (gameBoardElem)=>{
     })
 }
 
+// collision with food 
+export const checkFoodCollision = (foodPosition) =>{
+    return snakeBody.some((snakeSegment)=>{
+        return isCollision(snakeSegment, foodPosition);
+    })
+}
+// checking whether the food position and snake position are the same or not
+const isCollision = (snakeSegment, foodPosition)=>{
+    return snakeSegment.x === foodPosition.x &&
+    snakeSegment.y === foodPosition.y
+}
+
+// increase snake parts
+export const increaseSnakeParts = (SNAKE_INCREASE_RATE)=>{
+    snakeParts += SNAKE_INCREASE_RATE;
+}
+
 // snake head boundary check
 export const snakeHeadRect = ()=>{
     return snakeBody[0];
@@ -41,7 +56,9 @@ export const snakeHeadRect = ()=>{
 
 const updateSnakeSegments = ()=>{
     for(let i = 0; i < snakeParts; i++){
-        snakeBody.push(snakeBody[length - 1]);
-    }
+        snakeBody.push({...snakeBody[snakeBody.length - 1]});
+    };
+    // resetting snake parts to 0
+    snakeParts = 0;
 }
 
